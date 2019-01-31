d1bf945374bf279bf9a27faeecd01dcf523873fb
Raúl Cubitto <raulcubitto@gmail.com>
2019-01-31 18:14:53 -0300

    Import vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <template>
   <div id="app">
-    <h1 class="text-center mt-10">We're ready. Let's go.</h1>
+    <div>
+      <router-link to="/">Home</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
-
-<script>
-import "@/assets/css/styles.css";
-
-export default {
-  name: "app"
-};
-</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1 class="text-center mt-10">We're ready. Let's go!</h1>
+</template>
+
+<script>
+export default {
+  name: "home"
+};
+</script>
